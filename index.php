d001107f87e95aacbb95c65265f57629e1fefdd5
Dmitriy Vikulin <dvikulin@lachestry.com>
2022-07-28 15:31:54 +0300

    Filter data from parsedArray Make PSR standarts
    [#457]

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
+CONST CVS_HEADER = "\"city_ascii\",\"lat\",\"lng\",\"country\",\"iso2\",\"iso3\",\"population\"\n";
+
 function parseCsv (string $inputFilePointer)
 {
     if (file_exists ($inputFilePointer) ) {
-        echo "File exists\n";
+        echo "File $inputFilePointer exists\n";
 
         if (is_dir ('output') ) {
             echo "output exist\n";
@@ -17,29 +19,40 @@ function parseCsv (string $inputFilePointer)
 
         $parsedArray =array ();
         foreach ($parseArray as $string) {
-            $res = explode(",", $string);
-
-            foreach ($res as $i => $str) {
-                $res[$i] = trim($str, '"');
-            }
+            $res = explode (",", $string);
 
             $tempArray = array (
                 "city" => $res[0],
-                "lat" => (float) $res[1],
-                "lng" => (float) $res[2],
+                "lat" =>  (float) $res[1],
+                "lng" =>  (float) $res[2],
                 "country" => $res[3],
                 "iso2" => $res[4],
                 "iso3" => $res[5],
-                "pop" => (integer) $res[6]
+                "population" => (integer) $res[6]
             );
             array_push ($parsedArray, $tempArray);
         }
 
+        if (file_exists ("output/output_data_1.csv") ) {
+            unlink ("output/output_data_1.csv");
+            file_put_contents ("output/output_data_1.csv",CVS_HEADER);
+        }
+
+        foreach ($parsedArray as $i =>$element) {
+            $temp = explode (" ", $element["country"]);
+            if (count ($temp) > 1) {
+                $inputText = $element["city"].",".$element["lat"].",".$element["lng"].",".$element["country"].",".
+                    $element["iso2"].",".$element["iso3"].",".$element["population"]."\n";
+
+                file_put_contents ('output/output_data_1.csv', $inputText, FILE_APPEND);
+            }
+        }
+
     } else {
-        echo "File does not exists\n";
+        echo "File $inputFilePointer does not exists\n";
     }
 }
 
-//$inputData = '5_input_data_2.csv';
+$inputData = '5_input_data_2.csv';
 
-//parseCsv($inputData);
+parseCsv($inputData);
