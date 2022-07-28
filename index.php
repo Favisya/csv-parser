82d8ff4c1d67295023cde89da7effa24acf1143f
Dmitriy Vikulin <dvikulin@lachestry.com>
2022-07-28 13:05:26 +0300

    End work with parser csv Fix echo when it don't need
    [#457]

--- index.php
+++ index.php
@@ -1,23 +1,45 @@
 <?php
 
-function parseCsv(string $inputFilePointer)
+function parseCsv (string $inputFilePointer)
 {
-    if(file_exists($inputFilePointer))
-    {
+    if (file_exists($inputFilePointer)) {
         echo "File exists\n";
 
-        if(is_dir('output')){
+        if (is_dir ('output') ) {
             echo "output exist\n";
-        }else {
+        } else {
             echo "creating output directory\n";
-            mkdir('output',0777);
+            mkdir ('output',0777);
         }
 
-    }else {
+
+        $parseArray = file ($inputFilePointer,FILE_IGNORE_NEW_LINES);
+
+        $parsedArray =array ();
+        foreach ($parseArray as $string) {
+            $res = explode(",",$string);
+
+            foreach ($res as $i => $str) {
+                $res[$i] = trim($str,'"');
+            }
+
+            $tempArray = array (
+                "city" => $res[0],
+                "lat" => (float) $res[1],
+                "lng" => (float) $res[2],
+                "country" => $res[3],
+                "iso2" => $res[4],
+                "iso3" => $res[5],
+                "pop" => (integer) $res[6]
+            );
+            array_push ($parsedArray,$tempArray);
+        }
+
+    } else {
         echo "File does not exists\n";
     }
 }
 
-$inputData = '5_input_data_2.csv';
+//$inputData = '5_input_data_2.csv';
 
-parseCsv($inputData);
+//parseCsv($inputData);
