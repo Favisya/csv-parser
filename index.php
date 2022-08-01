aba1d0d4ce904304dd143d275cfb3871a2811188
Dmitriy Vikulin <dvikulin@lachestry.com>
2022-08-01 11:53:49 +0300

    Split code to functions Optimize code Make PSR standarts
    [#457]

--- index.php
+++ index.php
@@ -14,34 +14,60 @@ function sortByPopulationDesc($a, $b)
     return ($a['population'] < $b['population']) ? -1 : 1;
 }
 
+function getFormattedPopulation(int $population): string
+{
+    $populationFormatted = null;
+
+    if ($population >= 1000000) {
+        $remainder = ($population / 1000000) % 1000000;
+        $populationFormatted = $remainder . 'млн ';
+    }
+    if ($population >= 1000) {
+        $remainder = $population / 1000 % 1000;
+        $populationFormatted = $populationFormatted . $remainder . 'тыс ';
+    }
+    if ($population >= 1) {
+        $remainder = $population % 1000;
+        if ($remainder != 0) {
+            $populationFormatted = $populationFormatted . $remainder;
+        }
+    }
+    return (string) $populationFormatted;
+}
+
+function makeDirectory(): void
+{
+    if (is_dir('output')) {
+        echo 'output exist' . PHP_EOL;
+    } else {
+        echo 'creating output directory' . PHP_EOL;
+        mkdir('output');
+    }
+}
+
 function parseCsv(string $inputFilePointer)
 {
-    $firstCounter  = 0;
-    $secondCounter = 0;
-    $thirdCounter  = 0;
-    $fourthCounter = 0;
-    $inputCounter  = 0;
-
+    $counters = [
+    'first'  => 0,
+    'second' => 0,
+    'third'  => 0,
+    'fourth' => 0,
+    'input'  => 0
+    ];
     if (!file_exists($inputFilePointer) ) {
-        echo "File $inputFilePointer does not exists".PHP_EOL;
+        echo "File $inputFilePointer does not exists" . PHP_EOL;
         return false;
     }
-    echo "File $inputFilePointer exists".PHP_EOL;
+    echo "File $inputFilePointer exists" . PHP_EOL;
 
-    if (is_dir('output')) {
-        echo 'output exist'.PHP_EOL;
-    } else {
-        echo 'creating output directory'.PHP_EOL;
-        mkdir('output', 0777);
-    }
+    makeDirectory();
 
     $parseData = file ($inputFilePointer, FILE_IGNORE_NEW_LINES);
     $parsedData = [];
 
     foreach ($parseData as $i => $element) {
-        $res = str_getcsv($element, ',');
-
-        $tempData = [
+        $res = str_getcsv($element);
+        $parsedData[] = [
             'city'       => $res[0],
             'lat'        => $res[1],
             'lng'        => $res[2],
@@ -50,108 +76,80 @@ function parseCsv(string $inputFilePointer)
             'iso3'       => $res[5],
             'population' => $res[6]
         ];
-
-        array_push($parsedData, $tempData);
     }
-    $firstLineCsv = $parsedData[0];
-    $inputCounter = count($parsedData);
+    $firstLineCsv = array_shift($parsedData);
+    $counters['input'] = count($parsedData);
 
     $filesToWrite = [FIRST_OUTPUT, SECOND_OUTPUT, THIRD_OUTPUT];
     foreach ($filesToWrite as $file) {
-        $fileOpen = fopen('output/'.$file, 'w');
+        $fileOpen = fopen('output/' . $file, 'w');
         fputcsv($fileOpen, $firstLineCsv);
         fclose($fileOpen);
     }
 
-
-    foreach ($parsedData as $i => $element) {
+    $fileOpen = fopen('output/' . FIRST_OUTPUT, 'a');
+    foreach ($parsedData as $element) {
         $temp = explode(' ', $element['country']);
         if (count($temp) > 1) {
-            $firstFile = fopen('output/'.FIRST_OUTPUT, 'a');
-            fputcsv($firstFile, $element);
-            fclose($firstFile);
-            $firstCounter++;
+            fputcsv($fileOpen, $element);
+            $counters['first']++;
         }
     }
+    fclose($fileOpen);
 
     $countryData = [];
     // 2_filter for city in Russia and sort by population in ASC
-    foreach ($parsedData as $i => $element) {
+    foreach ($parsedData as $element) {
         if ($element['country'] === 'Russia') {
-            array_push($countryData, $element);
+            $countryData[] = $element;
         }
     }
 
     usort($countryData,'sortByPopulationDesc');
 
-    foreach ($countryData as $i => $element) {
-        $fileOpen = fopen('output/'.SECOND_OUTPUT, 'a');
+    $fileOpen = fopen('output/' . SECOND_OUTPUT, 'a');
+    foreach ($countryData as $element) {
         fputcsv($fileOpen, $element);
-        fclose($fileOpen);
     }
-    $secondCounter = count($countryData);
+    fclose($fileOpen);
+    $counters['second'] = count($countryData);
 
     // 3_filter where lat or lng is minus
-    foreach ($parsedData as $i => $element) {
+
+    $fileOpen = fopen('output/' . THIRD_OUTPUT, 'a');
+    foreach ($parsedData as $element) {
         if ($element['lat'] < 0 || $element['lng'] < 0) {
-            $fileOpen = fopen('output/'.THIRD_OUTPUT, 'a');
             fputcsv($fileOpen, $element);
-            fclose($fileOpen);
-            $thirdCounter++;
+            $counters['third']++;
         }
     }
+    fclose($fileOpen);
 
     // 4_filter add new column population_formatted
     $populationFiled = ['populationFormatted' => 'population_formatted'];
     $firstLineCsv += $populationFiled;
 
-    $fileOpen = fopen('output/'.FOURTH_OUTPUT, 'w');
+    $fileOpen = fopen('output/' . FOURTH_OUTPUT, 'w');
     fputcsv($fileOpen, $firstLineCsv);
     fclose($fileOpen);
 
-    array_shift($parsedData);
-    foreach ($parsedData as $i => $element) {
-        $populationFormatted = null;
-
-        if ($element['population'] >= 1000000) {
-            $remainder = ($element['population'] / 1000000) % 1000000;
-            $populationFormatted = $remainder.'млн ';
-        }
-        if ($element['population'] >= 1000) {
-            $remainder = $element['population'] / 1000 % 1000;
-            $populationFormatted = $populationFormatted.$remainder.'тыс ';
-        }
-        if ($element['population'] >= 1) {
-            $remainder = $element['population'] % 1000;
-            if ($remainder != 0) {
-                $populationFormatted = $populationFormatted.$remainder;
-            }
-        }
-
-        $outputText = [
-            $element['city'],
-            $element['lat'],
-            $element['lng'],
-            $element['country'],
-            $element['iso2'],
-            $element['iso3'],
-            $element['population'],
-            $populationFormatted
-        ];
-
-        $fileOpen = fopen('output/'.FOURTH_OUTPUT, 'a');
-        fputcsv($fileOpen, $outputText);
-        fclose($fileOpen);
+    $fileOpen = fopen('output/' . FOURTH_OUTPUT, 'a');
+    foreach ($parsedData as $element) {
+        $populationFormatted = getFormattedPopulation((int) $element['population']);
+        $element[] = $populationFormatted;
+        fputcsv($fileOpen, $element);
     }
+    fclose($fileOpen);
+    $counters['fourth'] = count($parsedData);
 
-    $fourthCounter = count($parsedData);
+    $outputInfo  = 'input rows: ' . $counters['input'] . PHP_EOL;
+    $outputInfo .= 'OUTPUT' . PHP_EOL . FIRST_OUTPUT . ' rows: ' . $counters['first'] . PHP_EOL;
+    $outputInfo .= SECOND_OUTPUT . ' rows: ' . $counters['second'] . PHP_EOL;
+    $outputInfo .= THIRD_OUTPUT . ' rows: ' . $counters['third'] . PHP_EOL;
+    $outputInfo .= FOURTH_OUTPUT . ' rows: ' . $counters['fourth'] . PHP_EOL;
 
-    $outputText = 'input rows: '.$inputCounter.PHP_EOL;
-    $outputText = $outputText.'OUTPUT'.PHP_EOL.FIRST_OUTPUT.' rows: '.$firstCounter.PHP_EOL;
-    $outputText = $outputText.SECOND_OUTPUT.' rows: '.$secondCounter.PHP_EOL;
-    $outputText = $outputText.THIRD_OUTPUT.' rows: '.$thirdCounter.PHP_EOL;
-    $outputText = $outputText.FOURTH_OUTPUT.' rows: '.$fourthCounter.PHP_EOL;
+    file_put_contents('output/output_info.txt', $outputInfo);
+    echo 'Ready!' . PHP_EOL;
 
-    file_put_contents('output/output_info.txt', $outputText);
-    echo 'Ready!'.PHP_EOL;
+    return true;
 }
