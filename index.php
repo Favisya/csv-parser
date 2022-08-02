989b69d9039017d5330de1e90b30eaf3ebbe5f29
Dmitriy Vikulin <dvikulin@lachestry.com>
2022-08-02 17:02:11 +0300

    Decomposition to oop format
    [#483]

--- index.php
+++ index.php
@@ -1,155 +1,47 @@
 <?php
 
-CONST FIRST_OUTPUT  = 'output_data_1.csv';
-CONST SECOND_OUTPUT = 'output_data_2.csv';
-CONST THIRD_OUTPUT  = 'output_data_3.csv';
-CONST FOURTH_OUTPUT = 'output_data_4.csv';
+require_once 'FileHandler.php';
+require_once 'CsvHandler.php';
+require_once 'CsvFilter.php';
+require_once 'InfoHandler.php';
 
-function sortByPopulationDesc($a, $b)
+const FIRST_OUTPUT  = 'output_data_1.csv';
+const SECOND_OUTPUT = 'output_data_2.csv';
+const THIRD_OUTPUT  = 'output_data_3.csv';
+const FOURTH_OUTPUT = 'output_data_4.csv';
+
+function parseCsv(string $filePointer, string $directory)
 {
-    if ($a['population'] == $b['population']) {
-        return 0;
-    }
+    $csvObject  = new CsvFilter();
+    $data       = $csvObject->readFile($filePointer);
 
-    return ($a['population'] < $b['population']) ? -1 : 1;
-}
+    $parsedData = $csvObject->parse($data);
+    $header     = $csvObject->getHeader($parsedData);
 
-function getFormattedPopulation(int $population): string
-{
-    $populationFormatted = null;
+    array_shift($parsedData);
 
-    if ($population >= 1000000) {
-        $remainder = ($population / 1000000) % 1000000;
-        $populationFormatted = $remainder . 'млн ';
-    }
-    if ($population >= 1000) {
-        $remainder = $population / 1000 % 1000;
-        $populationFormatted = $populationFormatted . $remainder . 'тыс ';
-    }
-    if ($population >= 1) {
-        $remainder = $population % 1000;
-        if ($remainder != 0) {
-            $populationFormatted = $populationFormatted . $remainder;
-        }
-    }
-    return (string) $populationFormatted;
-}
+    $csvObject->initializationCsvFile($directory, FIRST_OUTPUT, $header);
+    $filteredData = $csvObject->FilterDataByCountrySplit($parsedData);
+    $csvObject->writeFile($directory, FIRST_OUTPUT, $filteredData);
 
-function makeDirectory(): void
-{
-    if (is_dir('output')) {
-        echo 'output exist' . PHP_EOL;
-    } else {
-        echo 'creating output directory' . PHP_EOL;
-        mkdir('output');
-    }
-}
+    $csvObject->initializationCsvFile($directory, SECOND_OUTPUT, $header);
+    $filteredData = $csvObject->FilterDataByCountry($parsedData, 'Russia');
+    $csvObject->writeFile($directory, SECOND_OUTPUT, $filteredData);
 
-function parseCsv(string $inputFilePointer)
-{
-    $counters = [
-    'first'  => 0,
-    'second' => 0,
-    'third'  => 0,
-    'fourth' => 0,
-    'input'  => 0
-    ];
-    if (!file_exists($inputFilePointer) ) {
-        echo "File $inputFilePointer does not exists" . PHP_EOL;
-        return false;
-    }
-    echo "File $inputFilePointer exists" . PHP_EOL;
+    $csvObject->initializationCsvFile($directory, THIRD_OUTPUT, $header);
+    $filteredData = $csvObject->FilterDataByLatOrLng($parsedData, 0);
+    $csvObject->writeFile($directory, THIRD_OUTPUT, $filteredData);
 
-    makeDirectory();
+    $populationField = ['populationFormatted' => 'population_formatted'];
+    $header += $populationField;
 
-    $parseData = file ($inputFilePointer, FILE_IGNORE_NEW_LINES);
-    $parsedData = [];
+    $csvObject->initializationCsvFile($directory, FOURTH_OUTPUT, $header);
+    $filteredData = $csvObject->allDataPopulationFormatted($parsedData);
+    $csvObject->writeFile($directory, FOURTH_OUTPUT, $filteredData);
 
-    foreach ($parseData as $i => $element) {
-        $res = str_getcsv($element);
-        $parsedData[] = [
-            'city'       => $res[0],
-            'lat'        => $res[1],
-            'lng'        => $res[2],
-            'country'    => $res[3],
-            'iso2'       => $res[4],
-            'iso3'       => $res[5],
-            'population' => $res[6]
-        ];
-    }
-    $firstLineCsv = array_shift($parsedData);
-    $counters['input'] = count($parsedData);
+    $infoObject = new InfoHandler();
+    $data = $infoObject->getInfoAboutFiles($directory, $filePointer);
+    $infoObject->writeFile($directory, 'infoAboutFiles.txt', $data);
 
-    $filesToWrite = [FIRST_OUTPUT, SECOND_OUTPUT, THIRD_OUTPUT];
-    foreach ($filesToWrite as $file) {
-        $fileOpen = fopen('output/' . $file, 'w');
-        fputcsv($fileOpen, $firstLineCsv);
-        fclose($fileOpen);
-    }
-
-    $fileOpen = fopen('output/' . FIRST_OUTPUT, 'a');
-    foreach ($parsedData as $element) {
-        $temp = explode(' ', $element['country']);
-        if (count($temp) > 1) {
-            fputcsv($fileOpen, $element);
-            $counters['first']++;
-        }
-    }
-    fclose($fileOpen);
-
-    $countryData = [];
-    // 2_filter for city in Russia and sort by population in ASC
-    foreach ($parsedData as $element) {
-        if ($element['country'] === 'Russia') {
-            $countryData[] = $element;
-        }
-    }
-
-    usort($countryData,'sortByPopulationDesc');
-
-    $fileOpen = fopen('output/' . SECOND_OUTPUT, 'a');
-    foreach ($countryData as $element) {
-        fputcsv($fileOpen, $element);
-    }
-    fclose($fileOpen);
-    $counters['second'] = count($countryData);
-
-    // 3_filter where lat or lng is minus
-
-    $fileOpen = fopen('output/' . THIRD_OUTPUT, 'a');
-    foreach ($parsedData as $element) {
-        if ($element['lat'] < 0 || $element['lng'] < 0) {
-            fputcsv($fileOpen, $element);
-            $counters['third']++;
-        }
-    }
-    fclose($fileOpen);
-
-    // 4_filter add new column population_formatted
-    $populationFiled = ['populationFormatted' => 'population_formatted'];
-    $firstLineCsv += $populationFiled;
-
-    $fileOpen = fopen('output/' . FOURTH_OUTPUT, 'w');
-    fputcsv($fileOpen, $firstLineCsv);
-    fclose($fileOpen);
-
-    $fileOpen = fopen('output/' . FOURTH_OUTPUT, 'a');
-    foreach ($parsedData as $element) {
-        $populationFormatted = getFormattedPopulation((int) $element['population']);
-        $element[] = $populationFormatted;
-        fputcsv($fileOpen, $element);
-    }
-    fclose($fileOpen);
-    $counters['fourth'] = count($parsedData);
-
-    $outputInfo  = 'input rows: ' . $counters['input'] . PHP_EOL;
-    $outputInfo .= 'OUTPUT' . PHP_EOL . FIRST_OUTPUT . ' rows: ' . $counters['first'] . PHP_EOL;
-    $outputInfo .= SECOND_OUTPUT . ' rows: ' . $counters['second'] . PHP_EOL;
-    $outputInfo .= THIRD_OUTPUT . ' rows: ' . $counters['third'] . PHP_EOL;
-    $outputInfo .= FOURTH_OUTPUT . ' rows: ' . $counters['fourth'] . PHP_EOL;
-
-    file_put_contents('output/output_info.txt', $outputInfo);
     echo 'Ready!' . PHP_EOL;
-
-    return true;
 }
