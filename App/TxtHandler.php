6687686e530f04409e58d3373ed5acdd822c57df
Dmitriy Vikulin <dvikulin@lachestry.com>
2022-08-08 17:31:55 +0300

    Make PSR standarts
    [#501]

--- App/TxtHandler.php
+++ App/TxtHandler.php
@@ -12,11 +12,11 @@ class TxtHAndler
         foreach ($data as $element) {
             $res = explode('|', $element);
             $parsedData[] = [
-                'city' => $res[0],
-                'lat' => $res[1],
-                'lng' => $res[2],
-                'country' => $res[3],
-                'iso' => $res[4],
+                'city'       => $res[0],
+                'lat'        => $res[1],
+                'lng'        => $res[2],
+                'country'    => $res[3],
+                'iso'        => $res[4],
                 'population' => $res[5]
             ];
         }
